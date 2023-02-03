4d5c9e2ccbe9081a4d8c3574282b9fb3618fcf11
Joe DeVivo <joe@devivo.com>
2023-02-03 09:45:49 -0700

    Upgrade to 1.7.2

--- flake.nix
+++ flake.nix
@@ -9,22 +9,22 @@
       binaries = {
         "x86_64-linux" = {
           pname = "turbo-linux-64";
-          sha256 = "1926a91gba32msnsb6plz7rpg28wfwin1b6p6x624c16hcman6qb";
+          sha256 = "0bqhg6ffr51hh2wlknc3q4h4zfs2hng6l5aziji0cf9ccy6992cd";
         };
         "x86_64-darwin" = {
           pname = "turbo-darwin-64";
-          sha256 = "08y3lg439yxaljwkqyjqk3wxlv0ab3v7jyq26sdqn0ahciqqf3d5";
+          sha256 = "0pmb3yj3041z3x5fzfay9sqw1qxkvpks1zn2y4nmbvn790dvbfsd";
         };
         "aarch64-linux" = {
           pname = "turbo-linux-arm64";
-          sha256 = "1lw2s7jb72pvr7vqjm8g75hq6m97slrrzwdw5lln5k67ms6nj3b6";
+          sha256 = "1am12px882d9bkfd54wrdvzrhr23zvszcha64np8cs9aq32gfa1k";
         };
         "aarch64-darwin" = {
           pname = "turbo-darwin-arm64";
-          sha256 = "018gh9ps2x1fkck9n78l3d8ax1j5glayddl5cs56r94nj6zj1dff";
+          sha256 = "1szv48jvvnjfld22m6sfspz56rmiclln4vpm9cp5gkd39cqyhrfn";
         };
       };
-      version = "1.7.0";
+      version = "1.7.2";
       supportedSystems =
         [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
