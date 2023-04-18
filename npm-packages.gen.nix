7f72a966b44c94c5ef5168c3ee49862f1b2db73b
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-04-18 14:29:40 +0100

    feat: update turbo 1.9.3

--- npm-packages.gen.nix
+++ npm-packages.gen.nix
@@ -3,34 +3,34 @@
 {
   "aarch64-linux" = {
     pname = "turbo-linux-arm64";
-    version = "v1.9.0";
+    version = "v1.9.3";
     src = {
-      sha256 = "0q9275h49b41vjfr9c60r2yi0q0sdix8c80qcjr04yv82cqihfp9";
-      url = "https://registry.npmjs.org/turbo-linux-arm64/-/turbo-linux-arm64-1.9.0.tgz";
+      sha256 = "086k3422d9n91bh6c5fmz6rradimnnhpj6v85ps47if8bkrrhhpb";
+      url = "https://registry.npmjs.org/turbo-linux-arm64/-/turbo-linux-arm64-1.9.3.tgz";
     };
   };
   "x86_64-linux" = {
     pname = "turbo-linux-64";
-    version = "v1.9.0";
+    version = "v1.9.3";
     src = {
-      sha256 = "1nr15kx3i5ardpafln1j3wzc8nrsll4i2pnpq10ma96414d492fb";
-      url = "https://registry.npmjs.org/turbo-linux-64/-/turbo-linux-64-1.9.0.tgz";
+      sha256 = "0fxlgjyg5gi81vqfnaas8hf1j5bwl35z7a9fdhxv317w70dbgbb0";
+      url = "https://registry.npmjs.org/turbo-linux-64/-/turbo-linux-64-1.9.3.tgz";
     };
   };
   "x86_64-darwin" = {
     pname = "turbo-darwin-64";
-    version = "v1.9.0";
+    version = "v1.9.3";
     src = {
-      sha256 = "1dzibxb8fj7zr90j1awpmqf2gg7vmxbh32vmyz38304z6fd540pk";
-      url = "https://registry.npmjs.org/turbo-darwin-64/-/turbo-darwin-64-1.9.0.tgz";
+      sha256 = "1ndidl1di8g2zqzg3qhf7h6np4kbvks38d29lxpny5m3fxysjvsw";
+      url = "https://registry.npmjs.org/turbo-darwin-64/-/turbo-darwin-64-1.9.3.tgz";
     };
   };
   "aarch64-darwin" = {
     pname = "turbo-darwin-arm64";
-    version = "v1.9.0";
+    version = "v1.9.3";
     src = {
-      sha256 = "1v0f3f7s2r4cqgm40d5f6167wgvfn64rz792vx6w8n8y7bq1xmwd";
-      url = "https://registry.npmjs.org/turbo-darwin-arm64/-/turbo-darwin-arm64-1.9.0.tgz";
+      sha256 = "06dx0yrmvrdj5bqbpwgdy69gin9cq9kl4vb7hl6d865kw5850p19";
+      url = "https://registry.npmjs.org/turbo-darwin-arm64/-/turbo-darwin-arm64-1.9.3.tgz";
     };
   };
 }
