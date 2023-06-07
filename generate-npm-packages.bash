54838ea4b88574ae55363793f6e76530760dc836
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-06-07 17:47:22 +0100

    feat: update turborepo v1.10.2

--- generate-npm-packages.bash
+++ generate-npm-packages.bash
@@ -12,7 +12,7 @@ set -eu -o pipefail
 # ./generate-npm-packages.bash > npm-packages.gen.nix
 #
 
-declare version="1.10.1"
+declare version="1.10.2"
 declare -A "hashes"
 declare -A "urls"
 declare -A packages=(
--- npm-packages.gen.nix
+++ npm-packages.gen.nix
@@ -3,34 +3,34 @@
 {
   "aarch64-linux" = {
     pname = "turbo-linux-arm64";
-    version = "v1.10.1";
+    version = "v1.10.2";
     src = {
-      sha256 = "1k0jlq21x8blg9m9d62dpccpvfrzxwqshyfskdy10gjqayxpp1dz";
-      url = "https://registry.npmjs.org/turbo-linux-arm64/-/turbo-linux-arm64-1.10.1.tgz";
+      sha256 = "1467x0l5i39v40m4fnhi8g3dypq7wcindx6mhsl295z7q9b7vz7b";
+      url = "https://registry.npmjs.org/turbo-linux-arm64/-/turbo-linux-arm64-1.10.2.tgz";
     };
   };
   "x86_64-linux" = {
     pname = "turbo-linux-64";
-    version = "v1.10.1";
+    version = "v1.10.2";
     src = {
-      sha256 = "1dgqrplq2shcabiscxns8fb9b8fn65xhhmic71vzk29ak3bfyamj";
-      url = "https://registry.npmjs.org/turbo-linux-64/-/turbo-linux-64-1.10.1.tgz";
+      sha256 = "0drxq05vsicxackfg6z0smfix3pzb3bg00cqcvichwj9xn1hb4yj";
+      url = "https://registry.npmjs.org/turbo-linux-64/-/turbo-linux-64-1.10.2.tgz";
     };
   };
   "x86_64-darwin" = {
     pname = "turbo-darwin-64";
-    version = "v1.10.1";
+    version = "v1.10.2";
     src = {
-      sha256 = "033bxl9r3cim7divg9kcd6agmbvrqklr92b109x8qfbpsvypfgfw";
-      url = "https://registry.npmjs.org/turbo-darwin-64/-/turbo-darwin-64-1.10.1.tgz";
+      sha256 = "0yjnlw1a9bl34dvwyzbxd1knjb1gr894dggm0nvhg10vgk1k9y77";
+      url = "https://registry.npmjs.org/turbo-darwin-64/-/turbo-darwin-64-1.10.2.tgz";
     };
   };
   "aarch64-darwin" = {
     pname = "turbo-darwin-arm64";
-    version = "v1.10.1";
+    version = "v1.10.2";
     src = {
-      sha256 = "0h739vbjfb6n05ydbpk263aa12hk6hp2ycjci7g36sz4ms33rqkn";
-      url = "https://registry.npmjs.org/turbo-darwin-arm64/-/turbo-darwin-arm64-1.10.1.tgz";
+      sha256 = "159qjm7ncqk18fwgrxam3ygmhq4m4zhgpi1c3wc83ix2vv5j8ags";
+      url = "https://registry.npmjs.org/turbo-darwin-arm64/-/turbo-darwin-arm64-1.10.2.tgz";
     };
   };
 }
