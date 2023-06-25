d7a3e8abbf7ee69434ab07d5aa82510676b505c4
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-06-25 15:11:19 +0100

    feat: update turbo to v1.10.6

--- npm-packages.gen.nix
+++ npm-packages.gen.nix
@@ -3,34 +3,34 @@
 {
   "aarch64-linux" = {
     pname = "turbo-linux-arm64";
-    version = "v1.10.5";
+    version = "v1.10.6";
     src = {
-      sha256 = "1h5pxkl6xymgf2hgw0pqn7gmrf9p7ckl3m5r28kc854mln1g3fg8";
-      url = "https://registry.npmjs.org/turbo-linux-arm64/-/turbo-linux-arm64-1.10.5.tgz";
+      sha256 = "0wv8xqkm8lmvrg7h7rmdgsxh29agh3wjz4pjxbl3gixw1ysdh7hr";
+      url = "https://registry.npmjs.org/turbo-linux-arm64/-/turbo-linux-arm64-1.10.6.tgz";
     };
   };
   "x86_64-linux" = {
     pname = "turbo-linux-64";
-    version = "v1.10.5";
+    version = "v1.10.6";
     src = {
-      sha256 = "0yjlibnn6fsxbj9384kgbmsp2am6kr0bwag546vjcwbz7a78zxx9";
-      url = "https://registry.npmjs.org/turbo-linux-64/-/turbo-linux-64-1.10.5.tgz";
+      sha256 = "0irkghcikb8w4ckshhymz15wpz793arfq0mg0kirbknqrz5lmaaq";
+      url = "https://registry.npmjs.org/turbo-linux-64/-/turbo-linux-64-1.10.6.tgz";
     };
   };
   "x86_64-darwin" = {
     pname = "turbo-darwin-64";
-    version = "v1.10.5";
+    version = "v1.10.6";
     src = {
-      sha256 = "0kdfpz36y03b8b9ngfds974kv1ak8bmafpgamka9zgk1cdck05di";
-      url = "https://registry.npmjs.org/turbo-darwin-64/-/turbo-darwin-64-1.10.5.tgz";
+      sha256 = "0zpza87y621q21m2av635qqibiwyv0xa4s0p8ra1y36q6k47bgrm";
+      url = "https://registry.npmjs.org/turbo-darwin-64/-/turbo-darwin-64-1.10.6.tgz";
     };
   };
   "aarch64-darwin" = {
     pname = "turbo-darwin-arm64";
-    version = "v1.10.5";
+    version = "v1.10.6";
     src = {
-      sha256 = "0jxyqpzj1rya4ifgwpp856s658r3l0cb51aixsvnhivhipcnm4wz";
-      url = "https://registry.npmjs.org/turbo-darwin-arm64/-/turbo-darwin-arm64-1.10.5.tgz";
+      sha256 = "0x5pn8nr72f133vxl31s8110016mlwxva8j5lh8bk5dwlrrna2s0";
+      url = "https://registry.npmjs.org/turbo-darwin-arm64/-/turbo-darwin-arm64-1.10.6.tgz";
     };
   };
 }
