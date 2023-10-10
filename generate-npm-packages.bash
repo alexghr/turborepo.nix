ac3db73d263639d2bfe6632a6a645a6f3730f7ea
Alex Gherghisan <alexghr@users.noreply.github.com>
2023-10-10 08:06:29 +0100

    feat: script always fetches latest turbo

--- generate-npm-packages.bash
+++ generate-npm-packages.bash
@@ -4,17 +4,19 @@ set -eu -o pipefail
 
 # Inspired by https://discourse.nixos.org/t/update-many-shas/5031/5
 #
-# Use this script to generate an appropriate npm-packages.gen.nix for a
-# specific Turborepo version. It prefetches the archives for each of
-# the systems and computes its sha256.
+# Use this script to generate an appropriate npm-packages.gen.nix for
+# the latest Turborepo version. It prefetches the archives for each of
+# the systems and computes each archive's sha256.
 #
-# Update the version variable below and run
+# Requires: jq
+# Run as:
 # ./generate-npm-packages.bash > npm-packages.gen.nix
 #
 
-declare version="1.10.12"
 declare -A "hashes"
 declare -A "urls"
+declare -A "versions"
+
 declare -A packages=(
   ["x86_64-linux"]="turbo-linux-64"
   ["x86_64-darwin"]="turbo-darwin-64"
@@ -23,15 +25,19 @@ declare -A packages=(
 )
 
 for system in "${!packages[@]}"; do
-  urls[$system]="https://registry.npmjs.org/${packages[$system]}/-/${packages[$system]}-${version}.tgz"
+  versions[$system]=$(curl -s "https://registry.npmjs.org/${packages[$system]}" | jq -r '."dist-tags".latest')
+  urls[$system]="https://registry.npmjs.org/${packages[$system]}/-/${packages[$system]}-${versions[$system]}.tgz"
   hashes[$system]=$(nix-prefetch-url "${urls[$system]}" 2> /dev/null)
 done
+
+# TODO should we check that all versions are the same?
 
 printf '# DO NOT MODIFY\n'
 printf '# This is a generated file. See %s for details\n' "${0}"
 printf '{\n'
 for system in "${!packages[@]}"; do
   pname="${packages[$system]}"
+  version="${versions[$system]}"
   hash="${hashes[$system]}"
   url="${urls[$system]}"
 
